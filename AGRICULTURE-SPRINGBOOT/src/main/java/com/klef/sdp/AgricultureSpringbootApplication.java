8f7df07b8f5f1b3ece92cb9dba0829269d0b4618
KHVS <2300032396@kluniversity.in>
2025-09-17 14:33:39 +0530

    SDP v1.0

---
+++ AGRICULTURE-SPRINGBOOT/src/main/java/com/klef/sdp/AgricultureSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.klef.sdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricultureSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgricultureSpringbootApplication.class, args);
+		System.out.println("Jai Kisann!!!");
+	}
+
+}
